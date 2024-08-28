5f537526ab37b29d8639eea75bd96b6a7ebdd6f1
murhaf2211 <orfali.murhaf@web.de>
2024-08-28 16:38:47 +0200

    Add Species Record and for every Animal

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -3,7 +3,7 @@ package org.example;
 public record Animal(
         String id,
         String name,
-        String species,
-        int age, Owner owner) {
-
+        Species specie,
+        int age,
+        Owner owner) {
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,29 +7,31 @@ public class Main {
         Owner owner2= new Owner("Jack", 43, "Adress 777");
         Owner owner3= new Owner("John", 55, "Adress 789");
 
+        Species species1= new Species("leo" , 5000);
+        Species species2= new Species("Tigra" , 10000);
+        Species species3= new Species("Dumbo" , 20000);
 
-        Animal lion = new Animal("11", "Lion", "Leo",5,owner1);
-        Animal lion2 = new Animal("11", "Lion", "Leo",5, owner2);
-        Animal tiger = new Animal("22", "Tiger", "Tigra",3, owner2);
-        Animal bear = new Animal("33", "Bear", "Balu",7, owner3);
-        Animal elephant = new Animal("44", "Elephant", "Dumbo",10, owner1);
-
+        Animal lion = new Animal("11", "Lion", species1,5,owner1 );
+        Animal lion2 = new Animal("11", "Lion", species1,5, owner2);
+        Animal tiger = new Animal("22", "Tiger", species2,3, owner2);
+        Animal bear = new Animal("33", "Bear", species2,7, owner3);
+        Animal elephant = new Animal("44", "Elephant", species3,10,owner1);
+        System.out.println("---------------");
         System.out.println(lion);
+        System.out.println(lion.owner());
+        System.out.println(lion.specie());
+        System.out.println("---------------");
         System.out.println(tiger);
         System.out.println(bear);
         System.out.println(elephant);
-
+        System.out.println("---------------");
         System.out.println(lion.equals(tiger));
         System.out.println(lion.equals(lion2));
     }
 }
-//Call the automatically generated methods in the Record and check
-// if they do what you expect (equals, toString).
-//Create an Owner Record that stores some properties of a pet owner (Name, Age, Address).
-//
-//Let each Animal have an Owner.
-//Create a Species Record that stores the properties of an animal species (Name, Food requirement in grams per day).
-//
+
+
+
 //Let each Animal have a Species.
 //
 //In your main method, let multiple Animal objects have the same Species.
